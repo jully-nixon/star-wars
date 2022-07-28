9725b0f66901d90f1fd70036bef459d383e7b383
jully-nixon <jully.nixon.tenorio@accenture.com>
2022-07-28 11:55:09 -0300

    Componente Footer criado.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <label className='footer-title'>Desenvolvido para o desafio frontend INFLOR</label>
+        </div>
+    )
+}
+
+export default Footer;
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import Header from '../../components/Header';
 import Banner from '../Banner';
+import Footer from '../../components/Footer';
 
 const Home = () => {
+    const [color] = useState('#040608');
+
+    useEffect(() => {
+        document.body.style.backgroundColor = color;
+    }, [color]);
+
     return (
-        <div className='home'>
+        <>
             <Header />
             <Banner />
 
             <h2 className='home-title'>Personagens Saga Star Wars</h2>
-        </div>
+
+            <Footer />
+        </>
     );
 }
 
